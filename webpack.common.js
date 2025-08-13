5bafc9b48e4657506a7b2608e9944b4f92b5f82a
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-08-13 00:27:22 +0000

    Fix incorrect html plugin code

--- webpack.common.js
+++ webpack.common.js
@@ -24,11 +24,7 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html', // your HTML template
-    }),
-  ],
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
