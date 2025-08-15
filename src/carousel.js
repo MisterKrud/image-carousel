14cb45a6d34d151e5d38b984aa2794d613b27e6c
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-08-15 21:23:55 +1000

    Add pictures to src drive

--- src/carousel.js
+++ src/carousel.js
@@ -1,6 +1,8 @@
 import { images } from "./images";
+import { slideForward } from "./slideMovement";
 
 export const carousel = () => {
+
 
     const content = document.getElementById('content');
     //Div container framework
@@ -26,28 +28,55 @@ export const carousel = () => {
         imgDiv.classList.add('img-div')
         imgDiv.id = `img-${idx}`
         wideContainer.appendChild(imgDiv);
+        if (idx == 2) {
+            imgDiv.classList.add('visible')
+        }
     });
 
-
+    //Render 'Next' and 'Prev' buttons
     const buttonDiv = document.createElement('div');
         buttonDiv.id = 'button-div'
         
-    const fwdButton = document.createElement('button');
-        fwdButton.id = 'fwd-button';
-        fwdButton.classList.add('btn')
-        fwdButton.innerText = 'Next';
+   
     const bkButton = document.createElement('button');
         bkButton.id = 'bk-button';
         bkButton.classList.add('btn');
         bkButton.innerText = 'Prev';
 
+         const fwdButton = document.createElement('button');
+        fwdButton.id = 'fwd-button';
+        fwdButton.classList.add('btn')
+        fwdButton.innerText = 'Next';
+
     content.appendChild(buttonDiv);
-    buttonDiv.append(fwdButton, bkButton);
+    buttonDiv.append(bkButton, fwdButton);
+
+//    const visibleImage = imageData.imageArray[2]
+//    console.log(visibleImage)
+//     const visibleImageIndex = imageData.imageArray.indexOf(visibleImage);
+//     console.log(visibleImageIndex)
+    
+//      const slideForward = () =>{
+//         const visibleImage = imageData.imageArray[2]
+//         console.log(visibleImage)
+//         const visibleImageIndex = imageData.imageArray.findIndex(visibleImage);
+        
+//         visibleImage.classList.remove('visible');
+//         const nextVisibleImage = imageData.imageArray[visibleImageIndex+1];
+//         nextVisibleImage.classList.add('visible');
+    // }
 
 
-    const slideForward = () =>{
 
-    }
+
+    
+   
+    fwdButton.addEventListener('click', slideForward);
+
+ 
+    
+
+  
 
 return pictureFrame;
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { carousel } from "./carousel";
+import { slideForward } from "./slideMovement";
 import "./styles.css"
 const content = document.getElementById('content0;')
 carousel();
